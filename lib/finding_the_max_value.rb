04ebdca1a830e69e4b42bbf65a5c1f7cddb9115b
Christian P Andrews <cpandrews91@gmail.com>
2020-07-27 02:00:46 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,3 +1,16 @@
-def find_max_value(array)
-  # Add your solution here
-end
+
+Array = [1,2,1,3,4,3,5,4,3,2,1]
+
+def find_max_value(Array)
+    count = 0
+    max_value = -1
+while count < array.length do
+  if max_value < array[count]
+    max_value = array[count]
+elsif return max_value
+  end
+    count+=1
+  end 
+  puts max_value
+  end
+  find_max_value
